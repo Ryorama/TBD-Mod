dbcbb39ad3d01526a7741a28f09b2f1875c5b79d
Ryan <redryma@icloud.com>
2022-01-20 08:20:16 -0500

    Ree

---
+++ src/main/java/com/ryorama/tbd/TBD.java
@@ -0,0 +1,13 @@
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TBD implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
